b9f5253c88e7996d231fae1ca0171d5d916539d1
Артём Логинов < >
2017-03-20 20:42:15 +0300

    Removed unnecessary logs

--- backend/WSserver.js
+++ backend/WSserver.js
@@ -8,45 +8,41 @@ var clients = {};
 
 // WebSocket-сервер на порту 3000
 var webSocketServer = new WebSocketServer.Server({
-    port: 3000
+	port: 3000
 });
 webSocketServer.on('connection', function (ws) {
 
-    var client = url.parse(ws.upgradeReq.url, true).query
-    clients[client.login] = ws;
-    console.log("Новое соединение: " + client.login);
+	var client = url.parse(ws.upgradeReq.url, true).query
+	clients[client.login] = ws;
+	console.log("Новое соединение: " + client.login);
 
-    for (var key in clients) {
-        if (key != client.login) {
-            clients[key].send("Подключился логин: " + client.login);
-        }
-    }
+	for (var key in clients) {
+		if (key != client.login) {
+			clients[key].send("Подключился логин: " + client.login);
+		}
+	}
 
-    ws.on('message', function (message) {
+	ws.on('message', function (message) {
 
-        var parsedMsg = JSON.parse(message);
+		var parsedMsg = JSON.parse(message);
 
-        if (parsedMsg.message != undefined) {
-            console.log('Получено сообщение: ' + parsedMsg.message);
-            
-            var messageSender;
-            console.log(messageSender);
-			console.log(ws.upgradeReq.url);
+		if (parsedMsg.message != undefined) {
+			console.log('Получено сообщение: ' + parsedMsg.message);
+
+			var messageSender;
 			messageSender = url.parse(ws.upgradeReq.url, true).query.login;
-            console.log("Ищем отправителя: " + messageSender);
 
-            for (var key in clients) {
-				console.log("Указываем отправителя: " + messageSender);
-                console.log("Отправляю сообщение: \"" + messageSender + ": " + parsedMsg.message + "\"");
-                clients[key].send(messageSender + ": " + parsedMsg.message);
-            }
-        }
+			for (var key in clients) {
+				console.log("Отправляю сообщение: \"" + messageSender + ": " + parsedMsg.message + "\"");
+				clients[key].send(messageSender + ": " + parsedMsg.message);
+			}
+		}
 
-    });
+	});
 
-    ws.on('close', function () {
-        console.log('Соединение закрыто: ' + client.login);
-        delete clients[client.login];
-    });
+	ws.on('close', function () {
+		console.log('Соединение закрыто: ' + client.login);
+		delete clients[client.login];
+	});
 
 });
